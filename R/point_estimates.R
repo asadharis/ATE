a286fa26a3fc4675b6b5d05b40e48f7df0e9c61b
asadharis <asad.haris5862@gmail.com>
2015-09-08 01:35:09 -0700

    V 0.3.2 code commented and tested

--- R/point_estimates.R
+++ R/point_estimates.R
@@ -1,19 +1,23 @@
-#We note that we combine the newton raphson for both the simple estimate
-#and the treatment effect of the treated since there is only a small modification
-#needed.
+###################NOTE#############################
+#For simplicity we use three different functions for
+#the three different cases
+#1. Binary simple estimate
+#2. Binary treatment for ATT
+#3. Multiple treatment arms
+###################NOTE##############################
+#####################################################
 
-
-#########################################################################
-#Main function to obtain the simple point estimate
+#CASE 1: Binary treatment. Simple case, i.e. not ATT
 get.est.simple<- function(ini1,ini2, X, Y, Ti, theta, max.iter = 100,
                           tol = 1e-3,  bt.a = 0.3, bt.b = 0.5,
                           verbose = TRUE){
 
+  #initialize some objects
   N<- length(Y)
-
   umat<- cbind(1,X)
   ubar <- colMeans(umat)
 
+  #Perform some simple checks
   if(length(ini1) != length( umat[1,] ) ){
     stop("Incorrect length of initial vector")
   }
@@ -25,7 +29,8 @@ get.est.simple<- function(ini1,ini2, X, Y, Ti, theta, max.iter = 100,
     cat("Running BFGS algorithm for estimating Weights p: \n")
   }
 
-
+  #Implement the BFGS algorithm to get the optimal lambda_p
+  #and correspoding weights \hat{p}_k
   p.hat<- cpp_quasi_newt(ini1,umat, ubar, Ti, theta, bt.a,bt.b,
                  max.iter , tol)
 
@@ -33,25 +38,31 @@ get.est.simple<- function(ini1,ini2, X, Y, Ti, theta, max.iter = 100,
     cat("\nRunning BFGS algorithm for estimating Weights q: \n")
   }
 
+  #Implement the BFGS algorithm to get the optimal lambda_q
+  #and correspoding weights \hat{q}_k
   q.hat<- cpp_quasi_newt(ini2,umat, ubar, 1 - Ti, theta, bt.a,bt.b,
                          max.iter, tol)
 
+  #Obtain estimates for E[Y(1)], E[Y(0)] and tau = E[Y(1)] - E[Y(0)]
   Y_one <-  sum((p.hat$weights*Y)[Ti==1])
   Y_zero<- sum((q.hat$weights*Y)[Ti==0])
   tau.hat<-  Y_one - Y_zero
 
+  #Obtain weights and lambda's
   w.p<- p.hat$weights
   w.q<- q.hat$weights
 
   lam.p<- p.hat$res
   lam.q<- q.hat$res
 
+  #Send warning if algorithm did not converge
   conv = TRUE
   if(!p.hat$Conv | !q.hat$Conv){
     warning("BFGS Algorithm did not converge for atleast one objective function.")
     conv<- FALSE
   }
 
+  #Return list of objects
   res.l<- list("lam.p" = lam.p, "lam.q" = lam.q, "weights.p" = w.p,
                "weights.q" = w.q, "Y1" = Y_one, "Y0"= Y_zero,"tau" = tau.hat,
                "conv" = conv)
@@ -64,9 +75,11 @@ get.est.ATT<- function(ini2, X, Y, Ti, theta, max.iter = 100,
                        tol = 1e-3,  bt.a = 0.3, bt.b = 0.5,
                        verbose = TRUE){
 
+  #Initialize
   N<- length(Y)
   umat<- cbind(1,X)
 
+  #Main difference is that now u_bar chnages
   umat2<- umat[Ti==1,]
   ubar <- colMeans(umat2)
 
@@ -74,26 +87,32 @@ get.est.ATT<- function(ini2, X, Y, Ti, theta, max.iter = 100,
     stop("Incorrect length of initial vector")
   }
 
-
   if(verbose){
     cat("\nRunning BFGS algorithm Raphson for estimating Weights q: \n")
   }
+
+  #Obtain lambda'_q and \hat{q}_K for the case of ATT
   q.hat<- cpp_quasi_newt(ini2,umat, ubar, 1 - Ti, theta, bt.a,bt.b,
                          max.iter, tol)
 
+  #Note that treatment effect on treated is simple to estimate
   Y_one <- mean(Y[Ti==1])
+  #The calibration estimator for E[Y(0)|T = 1]
   Y_zero<- sum((q.hat$weights*Y)[Ti==0])
   tau.hat<-  Y_one - Y_zero
 
+  #Weights and lambda vectors
   w.q<- q.hat$weights
   lam.q<- q.hat$res
 
+  #Warning message if the algorithm did not converge
   conv = TRUE
   if(!q.hat$Conv){
     warning("\nBFGS algorithm did not converge for the objective function.")
     conv<- FALSE
   }
 
+  #Return list
   res.l<- list("lam.q" = lam.q, "weights.q" = w.q,
                "Y1" = Y_one, "Y0"= Y_zero,"tau" = tau.hat,
                "conv" = conv)
@@ -102,10 +121,12 @@ get.est.ATT<- function(ini2, X, Y, Ti, theta, max.iter = 100,
 
 ###############################################################################
 #Main function to obtain the point estimate for multiple treatment effect
+#Much of the syntax is the same as for the binary case
 get.est.MT<- function(ini.mat, X, Y, Ti, theta, max.iter = 100,
                       tol = 1e-3,  bt.a = 0.3, bt.b = 0.5,
                       verbose = TRUE){
 
+  #initialize some objects
   N<- length(Y)
   J<-length(unique(Ti))
   umat<- cbind(1,X)
@@ -122,25 +143,30 @@ get.est.MT<- function(ini.mat, X, Y, Ti, theta, max.iter = 100,
   weights.mat<- matrix(0, ncol = N, nrow = J  )
   Yj.mat<- numeric( J )
 
+  #Loop through the treatment arms
   for(j in 0:(J-1 ) ){
+    #Inidicator of treatment arm being treamtent j
     temp.Ti<- 1*(Ti==j)
 
+    #Implement BFGS algorithm
     pj.hat<- cpp_quasi_newt(ini.mat[j+1,],umat, ubar, temp.Ti, theta, bt.a,bt.b,
                            max.iter, tol)
 
+    #Find estimate for  E[Y(j)]
     Yj.hat <-  sum((pj.hat$weights*Y)[temp.Ti==1])
 
     Yj.mat[j+1]<- Yj.hat
     lam.mat[j+1,]<- pj.hat$res
     weights.mat[j+1,] <- pj.hat$weights
 
+    #Warning for non-convergence
     conv = TRUE
     if(!pj.hat$Conv){
       warning("BFGS algorithm did not converge for atleast one objective function.")
       conv<- FALSE
     }
   }
-
+  #Rerutn result.
   res.l<- list("lam.mat" = lam.mat, "weights.mat" = weights.mat,
                "Yj.hat" = Yj.mat, "conv" = conv)
   return(res.l)
--- R/variance_estimates.R
+++ R/variance_estimates.R
@@ -1,30 +1,34 @@
+###################NOTE#############################
+#As for point estimates, we consider the three different
+#cases oneat a time.
+###################NOTE##############################
 
+#We largely use the Notation of the Package Vignette
 
-#########################################################
 #########################################################
 #We begin with the estimating equations for the simple estimate
 get.gk.simple<- function(u,Y,Ti,lam,beta,tau1,tau0,theta,ulam,ubeta){
-
-  #N<- length(Y)
-  #ulam<- u%*%lam
-  #ubeta<- u%*%beta
-
+  #gk1 is the EE for lambda_p
   temp1<- Ti*cpp_dcr_rho(ulam,theta)
   gk1<- apply(u,2,"*", temp1)-u
 
+  #The EE for lambda_q
   temp2<- (1-Ti)*cpp_dcr_rho(ubeta,theta)
   gk2<- apply(u,2,"*", temp2)-u
 
+  #The EE for tau1, the estimate for E[Y(1)]
   gk3<- (Ti*Y)*cpp_dcr_rho(ulam,theta) - tau1
+  #The EE for tau0, the estimate for E[Y(0)]
   gk4<- ((1-Ti)*Y)*cpp_dcr_rho(ubeta,theta) - tau0
 
   cbind(gk1,gk2,gk3,gk4)
 }
 
 
-#This function obtains the big covariance matrix for all coefficients
-#THe bottom right element of this will be the variance estimate for tau-simple
+#This function obtains the big covariance matrix for the parameter theta
+#of Section 2.4
 get.cov.simple<- function(u, Y, Ti, obj,theta){
+  #Initialize some objects
   N<- length(Y)
   n1<- sum(Ti)
   lam<- (obj$lam.p)
@@ -36,10 +40,13 @@ get.cov.simple<- function(u, Y, Ti, obj,theta){
   tau0<- obj$Y0
   K<- length(lam)
 
+  #Obtain the estimating equations
   gk<- get.gk.simple(u,Y,Ti,lam,beta,tau1,tau0,theta,ulam,ubeta)
-
+  #Calculate the MEAT for the Huber-White sandwich estimate
   meat<- crossprod(gk)/N
 
+  #Again, we follow the notation of the package Vignette
+  #First we create the 2K*2K matrix A
   temp1<- Ti*cpp_ddcr_rho(ulam, theta)
   temp2 <- (1-Ti)*cpp_ddcr_rho(ubeta, theta)
   #We define the matrix A
@@ -49,43 +56,50 @@ get.cov.simple<- function(u, Y, Ti, obj,theta){
   A2<- crossprod(tempA2,u)/N
   A<- bdiag(A1,A2)
 
+  #Now for the 2*2K matrix C
   C1<- crossprod(u,temp1*Y)
   C2<- crossprod(u,temp2*Y)
   C<- t(bdiag(C1,C2))/N
 
+  #Calculate the bread of the sandwich estimator using
+  #block matrix inverse formula
   Ainv<- bdiag(solve(A1),solve(A2))
   tempMat<- Matrix(0,ncol = 2, nrow = 2*K)
   bread<- cbind( rbind(Ainv,C%*%Ainv), rbind(tempMat, -diag(rep(1,2)))  )
 
+  #Finally return the covariance matrix
   (bread%*% tcrossprod(meat,bread))/N
 }
 
 
 ###############################################################################
 
+#The EE equations for estimate the average treatment effect on
+#the treated. ATT
 get.gk.ATT<- function(u,Y,Ti,beta,tau1,tau0,theta,ubeta){
 
   n1<- sum(Ti)
   N<- length(Y)
   frac<-n1/N
-  #ubeta<- u%*%beta
 
+  #The notation is slightly mixed here compared to package Vignette
   temp1<-  (1-Ti)*cpp_dcr_rho(ubeta, theta)
   gk1<- apply(u,2,"*", temp1) - apply(u,2,"*", Ti)/frac
-  #gk1<- (1-Ti)*rho1(crossprod(be, uk),...)*uk - 1/frac*Ti.int*uk
+  #THe EE for expected value of E[Y(1)|T=1]
   gk2<- Ti*(Y -  tau1)/frac
-  #gk2<- N/n1*(Ti.int*(Y.scaler- tau1))
+  #THe EE for expected value of E[Y(0)|T=1]
   gk3<- temp1*Y - tau0
-  #gk3<- (1-Ti.int)*rho1(crossprod(be, uk),...)*Y.scaler - tau0
+  #THe EE for expected value E[T]
   gk4<- Ti-frac
 
+  #This fixes the ordering to agree with package vignette
   cbind(gk1,gk4,gk2,gk3)
 }
 
 
 #This function obtains the big covariance matrix for all coefficients for ATT
-#THe bottom right element of this will be the variance estimate for tau-ATT
 get.cov.ATT<- function(u, Y, Ti, obj,theta){
+  #We largely follow the same format as above
   N<- length(Y)
   n1<- sum(Ti)
   frac<-n1/N
@@ -97,18 +111,22 @@ get.cov.ATT<- function(u, Y, Ti, obj,theta){
   tau0<- obj$Y0
   K<- length(beta)
 
+  #Obtain gk and meat for sandwich estimator
   gk<- get.gk.ATT(u,Y,Ti,beta,tau1,tau0,theta,ubeta)
   meat<- crossprod(gk)/N
 
+  #Obatain the (K+1)*(K+1) matrix A
   temp1 <- (1-Ti)*cpp_ddcr_rho(ubeta, theta)
   tempA<- apply(u,2,"*",temp1)
   A<- cbind(crossprod(tempA,u) , crossprod(u,Ti)/(frac^2))/N
   A<- rbind(A,c(rep(0,K),-1) )
 
+  #Evaluate matrix C of dimension 2*(K+1)
   C<- Matrix(0, ncol = K, nrow = 2)
   C[2,]<- crossprod(u,temp1*Y)
   C<- cbind(C,c(0,0))/N
 
+  #Evaluate the bread matrix and find covariance est
   Ainv<- solve(A)
   tempMat<- Matrix(0,ncol = 2, nrow = K+1)
   bread<- cbind( rbind(Ainv,C%*%Ainv), rbind(tempMat, -diag(rep(1,2))) )
@@ -119,12 +137,14 @@ get.cov.ATT<- function(u, Y, Ti, obj,theta){
 
 ###############################################################################
 
-
+#The EE equations for multiple treatment arms
 get.gk.MT<- function(u,Y,Ti,lams, taus,theta, ulams ){
   J<- nrow(lams)
   gk1list<- vector("list", J)
   gk2list<- vector("list", J)
 
+  #For each treatment arm we obtain the EE for lamba_j and
+  #tau_j  = E[Y(j)]
   for(j in 1:J){
     ulam<- ulams[,j]
     temp1<- 1*(Ti== j-1)*cpp_dcr_rho(ulam,theta)
@@ -132,11 +152,9 @@ get.gk.MT<- function(u,Y,Ti,lams, taus,theta, ulams ){
     gk2list[[j]]<- 1*(Ti== j-1)*Y*cpp_dcr_rho(ulam,theta) - taus[j]
   }
   cbind(do.call(cbind, gk1list), do.call(cbind, gk2list) )
-
 }
 
 #This function obtains the BIG covariance matrix for all coefficients for MT
-#This time there is no bottom right element.
 get.cov.MT<- function(u, Y, Ti, obj,theta){
   N<- length(Y)
   lams<- obj$lam.mat
@@ -145,13 +163,15 @@ get.cov.MT<- function(u, Y, Ti, obj,theta){
   taus<- obj$Yj.hat
   ulams<- tcrossprod(u,lams)
 
+  #The meat matrix as usual
   gk<- get.gk.MT(u, Y, Ti, lams, taus, theta, ulams)
-
   meat<- crossprod(gk)/N
 
+  #Evaluate the A matrix of size JK*JK
   Alist<- vector("list", J)
   AinvList<- vector("list", J)
 
+  #And also the C matrix of size J*JK
   Clist<- vector("list", J)
   for(j in 1:J){
     ulam<- ulams[,j]
@@ -163,34 +183,42 @@ get.cov.MT<- function(u, Y, Ti, obj,theta){
     Clist[[j]] <- t(crossprod(u,temp1*Y)/N)
 
   }
+
   Ainv<- bdiag(AinvList)
   C<- bdiag(Clist)
   tempMat<- Matrix(0,ncol = J, nrow = J*K)
 
+  #Obtain the bread matrix
   bread<- cbind( rbind( Ainv ,C%*%Ainv ), rbind(tempMat, -diag(rep(1,J))))
   (bread%*%tcrossprod(meat, bread))/N
 }
 
 ###############################################################################
 
-
+#This function takes in an object given by the functions get.est.*
 estimate_variance<- function(object){
   K<-  object$K
   u<- cbind(1,object$X)
   if(object$gp == "simple"){
+    #get the full covariance matrix
     cov.mat<- get.cov.simple(u, object$Y, object$Ti, object, object$theta)
+    #Obtain the lower right corner 2*2 matrix for tau1 and tau0
     covEY<- cov.mat[-(1:(2*K) ),-(1:(2*K))]
     fin<- covEY
-    #fin[3,3]<- covEY[1,1]+covEY[2,2] - 2*covEY[1,2]
 
   }else if(object$gp == "ATT"){
+    #Again, we get the full cov matrix
     cov.mat<-  get.cov.ATT(u, object$Y, object$Ti,  object, object$theta)
 
+    #Again the lower right corner is for our relevant
+    #parameters
     covEY<- cov.mat[-(1:K),-(1:K)]
     covEY<- covEY[-1,-1]
     fin<- covEY
   }else{
+    #Full covariance matrix
     cov.mat<-  get.cov.MT(u, object$Y, object$Ti, object, object$theta)
+    #The lower right J*J submatrix is for our parameters
     covEY<- cov.mat[-(1:(object$J*K)),-(1:(object$J*K))]
     fin<- covEY
   }
